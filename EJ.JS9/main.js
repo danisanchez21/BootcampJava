a9a9d709f9e87450c956edd99cd8a6132b1ebc6b
Daniel Sanchez Aránega <sanchez126e@gmail.com>
2025-03-14 14:28:06 +0100

    Realizo ej9 JS

--- EJ.JS9/main.js
+++ EJ.JS9/main.js
@@ -7,3 +7,24 @@ function letras (texto) {
 }
 const resultado = letras("Hola, me llamo Dani");
 console.log(resultado);
+
+
+// -----------------------------------------------//
+
+
+function empiezaPorAca(cadena) {
+    return cadena.startsWith("aca");
+}
+
+
+console.log(empiezaPorAca("academia")); // true
+console.log(empiezaPorAca("escuela"));  // false
+
+// -----------------------------------------------//
+
+function saludarTresVeces(saludo) {
+    console.log(saludo.repeat(3));
+}
+
+saludarTresVeces("Hola ");
+
