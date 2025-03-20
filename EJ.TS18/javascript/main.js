f8e8afb3104104e329322678c302c31014b6b1cc
Daniel Sanchez Aranega <sanchez126e@gmail.com>
2025-03-20 15:52:31 +0100

    Cambios TS EJ

--- EJ.TS18/javascript/main.js
+++ EJ.TS18/javascript/main.js
@@ -1,1 +1,26 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const interruptor = document.getElementById("interruptor");
+    const bombilla = document.getElementById("bombilla");
+    if (!interruptor || !bombilla)
+        return;
+    const apagadoBtn = "./images/boff.jpg";
+    const encendidoBtn = "./images/bon.jpg";
+    const apagadaBomb = "./images/off.jpg";
+    const encendidaBomb = "./images/on.jpg";
+    let estadoEncendido = false;
+    interruptor.addEventListener("click", () => {
+        estadoEncendido = !estadoEncendido;
+        //MANERA ALTERNATIVA:
+        // interruptor.src = estadoEncendido ? encendidoBtn : apagadoBtn;
+        // bombilla.src = estadoEncendido ? encendidaBomb : apagadaBomb;
+        if (estadoEncendido) {
+            interruptor.src = encendidoBtn;
+            bombilla.src = encendidaBomb;
+        }
+        else {
+            interruptor.src = apagadoBtn;
+            bombilla.src = apagadaBomb;
+        }
+    });
+});
---
+++ EJ.TS18/main.ts
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded",() =>{
+
+const interruptor = document.getElementById("interruptor") as HTMLImageElement;
+const bombilla = document.getElementById("bombilla") as HTMLImageElement;
+
+if(!interruptor || !bombilla) return;
+
+const apagadoBtn = "./images/boff.jpg";
+const encendidoBtn ="./images/bon.jpg";
+const apagadaBomb  ="./images/off.jpg";
+const encendidaBomb  ="./images/on.jpg";
+
+let estadoEncendido = false;
+
+interruptor.addEventListener("click",()=>{
+
+    estadoEncendido = !estadoEncendido;
+
+    //MANERA ALTERNATIVA:
+    // interruptor.src = estadoEncendido ? encendidoBtn : apagadoBtn;
+    // bombilla.src = estadoEncendido ? encendidaBomb : apagadaBomb;
+
+    if (estadoEncendido) {
+        interruptor.src = encendidoBtn;
+        bombilla.src = encendidaBomb;
+    } else {
+        interruptor.src = apagadoBtn;
+        bombilla.src = apagadaBomb;
+    }
+});
+
+
+});
